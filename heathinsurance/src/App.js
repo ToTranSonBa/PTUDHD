7bfd76e0a2f124b8fffc084a763972d306c5c3f1
HoangCau <hoangcau14783@gmail.com>
2023-12-04 20:03:56 +0700

    initial react app

---
+++ heathinsurance/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LOGO</h1>
+    </div>
+  );
+}
+
+export default App;
