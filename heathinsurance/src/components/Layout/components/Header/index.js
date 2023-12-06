817cf02a93b2136983c3f637e0973880acc126af
HoangCau <hoangcau14783@gmail.com>
2023-12-07 00:32:57 +0700

    header css

--- heathinsurance/src/components/Layout/components/Header/index.js
+++ heathinsurance/src/components/Layout/components/Header/index.js
@@ -1,5 +1,7 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import companyLogo from '../../../../assets/image/Logo2.png';
+import { MdAccountCircle } from 'react-icons/md';
 
 const cx = classNames.bind(styles);
 
@@ -7,8 +9,23 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* {Logo} */}
-                {/* {Search} */}
+                <div className={cx('logo')}>
+                    <img src={companyLogo} alt="Logo" />
+                </div>
+                <nav className={cx('navbar')}>
+                    <li>Trang chủ</li>
+                    <li>Dịch vụ</li>
+                    <li>Giới thiệu</li>
+                </nav>
+                <div className={cx('right_navbar')}>
+                    <div>
+                        <button className={cx('login-btn')}>LogIn</button>
+                        <button className={cx('logup-btn')}>LogUp</button>
+                    </div>
+                    <div className={cx('account')}>
+                        <MdAccountCircle />
+                    </div>
+                </div>
             </div>
         </header>
     );
