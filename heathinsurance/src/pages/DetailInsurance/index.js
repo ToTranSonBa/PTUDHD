bcbe7096a44b615859061894307f0eb3fd301277
nthieu2212 <nthieu2212@gmail.com>
2024-01-20 16:54:23 +0700

    admin - xem chi tiet san pham bao hiem

--- heathinsurance/src/pages/DetailInsurance/index.js
+++ heathinsurance/src/pages/DetailInsurance/index.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
-import ImageUpload from './imageUpload';
+import { useNavigate } from 'react-router-dom';
 import { getInsuranceByIdApi } from '../../services/Admin/ApiInsurance/insurance';
 import { useParams } from 'react-router-dom';
 
 const DetailInsurance = () => {
+    
+    const navigate = useNavigate();
     const [insurance, setInsurance] = useState({});
     const { id } = useParams();
+    const imageUrl = 'https://cdna.artstation.com/p/assets/images/images/057/382/630/original/gilson-junior-sephirotcloudia.gif?1671462201';
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -20,43 +23,81 @@ const DetailInsurance = () => {
         };
         fetchData();
     }, [id]);
+    const handleAdd = () => {
+        //call API
+        navigate(`/admin/insurances/`);
+    };
+    const ProgramPrice = ({ program }) => (
+        <div key={program.programId}>
+          <li>{program.programName}: <span>${program.price}</span></li>
+        </div>
+    );
+    const Conditions = ({ conditions }) => (
+        <div>
+          <li>Conditions:
+            <ul>
+                {conditions.map((condition, index) => (
+                <li key={index}>
+                    {condition.name}: <span>{condition.question}</span> 
+                </li>
+                ))}
+            </ul>
+          </li>
+          
+        </div>
+      );
 
     return (
+        
         <div style={{ paddingLeft: '260px' }}>
-            <div style={{ height: '100px', width: '100%', textAlign: 'center', marginTop: '50px' }}>
-                <span style={{ fontSize: '25px', color: 'green', paddingTop: '50px', fontWeight: 'bold' }}>
-                    Thêm Bảo Hiểm
-                </span>
-            </div>
-            <div className="details">
-                <div className="big-img" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <ImageUpload />
+            <div className = "card-wrapper">
+                <div className = "mycard">
+                    {/* card left */}
+                    <div className = "product-imgs">
+                        
+                                    <img className="img-item"src = {imageUrl} alt = "Product Image"/>
+                                
+                            
+                    </div>
+                    {/* card right */}
+                <div className = "product-content">
+                    <h2 className = "product-title">{insurance.policyName}</h2>
+                    
+                    <div className = "product-detail">
+                        <h2 className='descrip'style={{paddingBottom: '0px'}}>Description: </h2>
+                        <p>{insurance.shortDescription}</p>
+                        <ul>
+                        <li>ID: <span>{insurance.productId}</span></li>
+                        <li>Thủ tục tham gia: <span>{insurance.participationProcedure}</span></li>
+                        <li>Phí đảm bảo: <span>{insurance.feeGuarantee}</span></li>
+                        <li>Cam kết: <span>{insurance.commitment}</span></li>
+                        <li>Bên tham gia: <span>{insurance.insuredParty}</span></li>
+                        <li>Phạm vi lãnh thổ: <span>insurance.territorialScope</span></li>
+                        
+                        <li>Giá:
+                        {insurance.programPrice &&
+                        insurance.programPrice.map((program) => (
+                        <ProgramPrice key={program.programId} program={program} />
+                        ))}
+                        </li>      
+                        <Conditions conditions={insurance.conditions || []} />
+                        <li>Số lượng đã bán: <span>{insurance.totalQuantitySold}</span></li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="box">
-                    <form>
-                        <div className="row">
-                            <label htmlFor="productName">Tên Bảo Hiểm</label>
-                            <input type="text" id="productName" name="name" value={insurance.policyName} />
-                        </div>
-                        <div className="row">
-                            <label htmlFor="productPrice">Price</label>
-                            <input type="number" id="productPrice" name="price" value={100} />
-                        </div>
-                        <div className="row">
-                            <label htmlFor="productDescription">Description</label>
-                            <textarea id="productDescription" name="description" value={insurance.shortDescription} />
-                        </div>
-                        <div className="row">
-                            <button type="submit" className="add">
-                                <span style={{ fontWeight: 'bold', color: 'white', fontSize: '15px' }}>
-                                    Thêm Bảo Hiểm
-                                </span>
-                            </button>
-                        </div>
-                    </form>
                 </div>
+                
+               
             </div>
-        </div>
+            <div className="d-grid gap-2 col-2 mx-auto">
+                    <button className="button-28" type="button" onClick={() => handleAdd()}>
+                        Trở về
+                    </button>      
+            </div>
+           
+         </div> 
+
+         
     );
 };
 
